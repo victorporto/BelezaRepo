d3573a279d38545c50036bedeb5e827ff4ad1c7e
victorporto <victorportolambert200@gmail.com>
2017-10-27 14:28:17 -0300

    Alterando a ordem de execução das Activities.
    - Agora a SplashScreenActivity é a activity inicial.

--- app/src/main/java/belezaapp/studiovictor/com/belezaapp/SplashScreenActivity.java
+++ app/src/main/java/belezaapp/studiovictor/com/belezaapp/SplashScreenActivity.java
@@ -1,5 +1,8 @@
 package belezaapp.studiovictor.com.belezaapp;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,20 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        //Esconde a 'ActionBar' da 'SplashScreenActivity'
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //Faz a transição com delay de 2 segundos.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginScreenActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
